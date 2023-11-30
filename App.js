891b943853e82113d68f3976722869c4e7227a38
Htoo Aung Lin <htooaunglin@yomabank.com>
2023-11-30 22:28:37 +0630

    updated

--- App.js
+++ App.js
@@ -1,11 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { TextInput, Button, HelperText } from 'react-native-paper';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    fatherName: '',
+    dob: '',
+    email: '',
+    address: '',
+    phoneNumber: '',
+  });
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    fatherName: Yup.string().required('Father Name is required'),
+    dob: Yup.string().required('Date of Birth is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    address: Yup.string().required('Address is required'),
+    phoneNumber: Yup.string().required('Phone Number is required'),
+  });
+
+  const initialValues = {
+    name: '',
+    fatherName: '',
+    dob: '',
+    email: '',
+    address: '',
+    phoneNumber: '',
+  };
+
+  const handleSubmit = (values) => {
+    setFormData(values); // Update formData state
+    // Handle the form submission logic here
+    console.log(values);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+          <>
+            <TextInput
+              label="Name"
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              value={values.name}
+            />
+            <HelperText type="error" visible={Boolean(errors.name)}>
+              {errors.name}
+            </HelperText>
+
+            <TextInput
+              label="Father Name"
+              onChangeText={handleChange('fatherName')}
+              onBlur={handleBlur('fatherName')}
+              value={values.fatherName}
+            />
+            <HelperText type="error" visible={Boolean(errors.fatherName)}>
+              {errors.fatherName}
+            </HelperText>
+
+            <TextInput
+              label="Date of Birth"
+              onChangeText={handleChange('dob')}
+              onBlur={handleBlur('dob')}
+              value={values.dob}
+            />
+            <HelperText type="error" visible={Boolean(errors.dob)}>
+              {errors.dob}
+            </HelperText>
+
+            <TextInput
+              label="Email Address"
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            <HelperText type="error" visible={Boolean(errors.email)}>
+              {errors.email}
+            </HelperText>
+
+            <TextInput
+              label="Address"
+              onChangeText={handleChange('address')}
+              onBlur={handleBlur('address')}
+              value={values.address}
+            />
+            <HelperText type="error" visible={Boolean(errors.address)}>
+              {errors.address}
+            </HelperText>
+
+            <TextInput
+              label="Phone Number"
+              onChangeText={handleChange('phoneNumber')}
+              onBlur={handleBlur('phoneNumber')}
+              value={values.phoneNumber}
+            />
+            <HelperText type="error" visible={Boolean(errors.phoneNumber)}>
+              {errors.phoneNumber}
+            </HelperText>
+
+            <Button mode="contained" onPress={handleSubmit}>
+              Submit
+            </Button>
+          </>
+        )}
+      </Formik>
       <StatusBar style="auto" />
+      <Text>Form Data: {JSON.stringify(formData)}</Text>
     </View>
   );
 }
@@ -14,7 +124,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 25,
   },
 });
